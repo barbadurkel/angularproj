dd3adfecb39084fe5e5a01663202783341ad24c2
moubella <sudofalcon@gmail.com>
2019-06-24 11:12:39 +0200

    added a dashboard view and link

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-    { path : 'heroes', component : HeroesComponent }
+    { path : 'heroes', component : HeroesComponent },
+    { path : 'dashboard', component : DashboardComponent},
+    { path : '', redirectTo : '/dashboard', pathMatch :'full'}
 ];
 
 @NgModule({
-      // initialization of the router and making it listen for browser location changes (routes)
+  // initialization of the router and making it listen for browser location changes (routes)
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
