904aa0204d2573f7304b6848145d7e1af8457ac3
moubella <sudofalcon@gmail.com>
2019-06-25 15:58:05 +0200

    added an async search heroes by name on the dashboard view

--- angularapp/src/app/hero.service.ts
+++ angularapp/src/app/hero.service.ts
@@ -66,6 +66,18 @@ private handleError<T> (operation = 'operation', result?: T){
           tap(_ => this.log(`updated hero id = ${hero.id}`)),
           catchError(this.handleError<any>('updateHero')));
   }  
+  // Get heroes whose name contains search term
+  searchHeroes(term: string): Observable <Hero[]>{
+      if(!term.trim()){
+        return of([]);
+      }
+      return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+        tap(_ => this.log(`found heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+
+  }
+
   private log(message: string){
       this.messageService.add(`HeroService: ${message}`);
   }
